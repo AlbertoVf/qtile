d5a7cae401d4817c15e9b07f8f89289453b3ceee
AlbertoVf <AlbertoVf97@protonmail.com>
2021-01-22 18:11:00 +0100

    Añadir nuevos temas

--- themes/themes.py
+++ themes/themes.py
@@ -1,37 +1,131 @@
 
-# def name_colors():
+# def name():
 #     return [
-#         ["#","#"], #color 0
-#         ["#","#"], #color 1
-#         ["#","#"], #color 2
-#         ["#","#"], #color 3
-#         ["#","#"], #color 4
-#         ["#","#"], #color 5
-#         ["#","#"], #color 6
-#         ["#","#"], #color 7
-#         ["#","#"], #color 8
-#         ["#","#"]  #color 9
+#         "#", #color 0
+#         "#", #color 1
+#         "#", #color 2
+#         "#", #color 3
+#         "#", #color 4
+#         "#", #color 5
+#         "#", #color 6
+#         "#", #color 7
+#         "#", #color 8
+#         "#"  #color 9
 #     ]
 
-def darcula_colors():
+def darcula():
     return [
-        "#292d3e", "#292d3e", "#4c566a", "#ffb86c", "#5e81ac", "#f1ffff", "#ff5555", "#62FF00", "#A77AC4", "#a9a9a9"
+        "#292d3e",
+        "#ffb86c",
+        "#5e81ac",
+        "#f1ffff",
+        "#ff5555",
+        "#62FF00",
+        "#A77AC4",
+        "#a9a9a9"
     ]
 
 
-def default_colors():
+def default():
     return [
-        "#2F343F", "#2F343F", "#c0c5ce", "#fba922", "#3384d0", "#f3f4f5", "#cd1f3f", "#62FF00", "#6790eb", "#a9a9a9"
+        "#2F343F",
+        "#2F343F",
+        "#c0c5ce",
+        "#fba922",
+        "#3384d0",
+        "#f3f4f5",
+        "#cd1f3f",
+        "#62FF00",
+        "#6790eb",
+        "#a9a9a9"
     ]
 
 
-def darkgrey_colors():
+def dark_grey():
     return [
-        "#0f101a", "#37383b", "#f1ffff", "#f1ffff", "#66818d", "#4c566a", "#3f575b", "#0f101a", "#334148", "#556a74"
+        "#0f101a",
+        "#37383b",
+        "#f1ffff",
+        "#f1ffff",
+        "#66818d",
+        "#4c566a",
+        "#3f575b",
+        "#0f101a",
+        "#334148",
+        "#556a74"
     ]
 
 
-def nord_colors():
+def nord():
     return [
-        "#212121", "#212121", "#f1ffff", "#f1ffff", "#81a1c1", "#4c566a", "#81a1c1", "#b48ead", "#88c0d0", "#ebcb8b"
+        "#212121",
+        "#212121",
+        "#f1ffff",
+        "#f1ffff",
+        "#81a1c1",
+        "#4c566a",
+        "#81a1c1",
+        "#b48ead",
+        "#88c0d0",
+        "#ebcb8b"
+    ]
+
+
+def rosepine():
+    return [
+        "#1f1d29",
+        "#ABB2BF",
+        "#1f1d29",
+        "#eabbb9",
+        "#f1ffff",
+        "#4c566a",
+        "#ea6f91",
+        "#eabbb9",
+        "#f1ca93",
+        "#9bced7"
+    ]
+
+
+def material_ocean():
+    return [
+        "#0f101a",
+        "#353c4a",
+        "#f1ffff",
+        "#0f101a",
+        "#f1ffff",
+        "#4c566a",
+        "#a151d3",
+        "#F07178",
+        "#fb9f7f",
+        "#ffd47e"
+    ]
+
+
+def one_dark():
+    return [
+        "#1e2127",
+        "#ABB2BF",
+        "#ffffff",
+        "#282C34",
+        "#f1ffff",
+        "#4c566a",
+        "#E06C75",
+        "#C678DD",
+        "#61AFEF",
+        "#98C379"
+    ]
+
+
+def indigo():
+    return [
+        "#140f07",
+        "#282d4d",
+        "#ffe8f4",
+        "#fffbf4",
+        "#6060a3",
+        "#a89df9",
+        "#8842c7",
+        "#0c3389",
+        "#0893ff",
+        "#0efff9",
     ]
