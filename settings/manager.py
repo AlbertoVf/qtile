c99be8d58ddafa7ae2578e479d4f45be6146281f
AlbertoVf <avf.git-repos@protonmail.com>
2024-05-13 18:23:46 +0200

    refactor: read theme

--- settings/manager.py
+++ settings/manager.py
@@ -12,7 +12,7 @@ class Theme:
     color2     = "color2"
     color3     = "color3"
     color4     = "color4"
-
+    tf         = set([background, foreground, active, inactive, color1, color2, color3, color4])
 
 getenv        = lambda key: load(open(f"{qtile_path}/.env", "r"))[key]
 config        = join(expanduser("~"), ".config")
@@ -26,24 +26,13 @@ mail          = getenv("mail")
 
 def theme_selector(theme=getenv("theme")) -> Theme:
     try:
-        if exists(join(qtile_themes, "themes.yaml")):
-            t = load(open(join(qtile_themes, "themes.yaml")))[theme]
-        elif exists(join(qtile_themes, f"{theme}.yaml")):
-            t = load(open(join(qtile_themes, f"{theme}.yaml")))
-        if not set(t.keys()).issubset(set([Theme.background, Theme.foreground, Theme.active, Theme.inactive, Theme.color1, Theme.color2, Theme.color3, Theme.color4])):
+        t = load(open(join(qtile_themes, "themes.yaml")))[theme] if exists(join(qtile_themes, "themes.yaml")) else load(open(join(qtile_themes, f"{theme}.yaml")))
+        if not set(t.keys()).issubset(Theme.tf):
             raise ValueError("Theme must have 8 values")
     except (ValueError, FileNotFoundError):
-        t = {
-            Theme.background : "#0f101a",
-            Theme.foreground : "#f1ffff",
-            Theme.active     : "#f1ffff",
-            Theme.inactive   : "#4c566a",
-            Theme.color1     : "#a151d3",
-            Theme.color2     : "#F07178",
-            Theme.color3     : "#fb9f7f",
-            Theme.color4     : "#ffd47e",
-        }
-    return {layout: color.replace("0x", "#") for layout, color in t.items()}
+        t = { Theme.background: "#0f101a", Theme.foreground: "#f1ffff", Theme.active: "#f1ffff", Theme.inactive: "#4c566a", Theme.color1: "#a151d3", Theme.color2: "#F07178", Theme.color3: "#fb9f7f", Theme.color4: "#ffd47e", }
+    return t
 
 
 theme = theme_selector()
+print(theme)
