944e7fb8e5b19c407eca92a92e55464c4f92ccbc
AlbertoVf <avf.git-repos@protonmail.com>
2024-05-26 17:24:39 +0200

    refactor: replace console
    change console to terminal

--- settings/manager.py
+++ settings/manager.py
@@ -1,38 +1,40 @@
 from os.path import join, expanduser, exists
-
 from toml import load, loads
 
-config        = join(expanduser("~"), ".config")
-qtile_path    = join(config, "qtile")
+qtile_path    = join(expanduser("~"), ".config", "qtile")
 qtile_scripts = join(qtile_path, "scripts")
 qtile_themes  = join(qtile_path, "themes")
-getenv        = lambda key: load(open(f"{qtile_path}/qtile.conf"))['USER'][key]
-terminal      = getenv("console")
-font          = getenv("font")
-mail          = getenv("mail")
+
+
+def getenv(key, config_name="USER"):
+    config = load(open(f"{qtile_path}/qtile.conf"))
+    return config.get(config_name, {}).get(key, config["DEFAULT"].get(key))
+
 
 class Theme:
-    background = "background"
-    foreground = "foreground"
-    active = "active"
-    inactive = "inactive"
-    color1 = "color1"
-    color2 = "color2"
-    color3 = "color3"
-    color4 = "color4"
+    background, foreground = "background", "foreground"
+    active, inactive = ( "active", "inactive")
+    color1, color2, color3, color4 = ( "color1", "color2", "color3", "color4" )
     tf = set([background, foreground, active, inactive, color1, color2, color3, color4])
+    file_theme = lambda file: join(qtile_themes, f"{file}.toml")
+
 
 def theme_selector(theme=getenv("theme")) -> Theme:
     try:
-        t = loads(open(join(qtile_themes, "themes.toml")).read())[theme] if exists(join(qtile_themes, "themes.toml")) else loads(open(join(qtile_themes, f"{theme}.toml")).read())
+        t = (
+            loads(open(Theme.file_theme("themes")).read())[theme]
+            if exists(Theme.file_theme("themes"))
+            else loads(open(Theme.file_theme(theme)).read())
+        )
         if not set(t.keys()).issubset(Theme.tf):
             raise ValueError("Theme must have 8 values")
     except (ValueError, FileNotFoundError):
-        t = { Theme.background: "#0f101a", Theme.foreground: "#f1ffff", Theme.active: "#f1ffff", Theme.inactive: "#4c566a", Theme.color1: "#a151d3", Theme.color2: "#F07178", Theme.color3: "#fb9f7f", Theme.color4: "#ffd47e" }
+        t = { Theme.background: "#0f101a", Theme.foreground: "#f1ffff", Theme.active: "#f1ffff", Theme.inactive: "#4c566a", Theme.color1: "#a151d3", Theme.color2: "#F07178", Theme.color3: "#fb9f7f", Theme.color4: "#ffd47e", }
     return t
 
 
-theme = theme_selector()
+mail,terminal = (getenv("mail"), getenv("terminal"))
+font, theme   = (getenv("font"), theme_selector())
 
 if __name__ == "__main__":
     print(theme)
